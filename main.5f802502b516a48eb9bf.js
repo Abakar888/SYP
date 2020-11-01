7e45d877dbf96d11f1763d494fe6296ae970f508
Абакар <akap302@gmail.com>
2020-11-01 21:32:04 +0300

    Updates

---
+++ main.5f802502b516a48eb9bf.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var e=document.querySelector(".block"),t=document.querySelector(".button"),u=document.querySelector("input");document.querySelector("out-name"),t.onclick=function(){e.innerHTML+=u.value+"<br>"}})();
