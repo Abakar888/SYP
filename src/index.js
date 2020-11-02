3b9dc81ae92e900ec7eb32b1282af66fecec829a
Абакар <akap302@gmail.com>
2020-11-02 03:31:40 +0300

    create random project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
 import './style.css'
 
+let block = document.querySelector('.block');
+let block2 = document.querySelector('.block2');
+let block3 = document.querySelector('.block3');
+let block4 = document.querySelector('.block4');
+let button = document.querySelector('.button');
+let input = document.querySelector('input');
+let outName = document.querySelector('.out-name');
+let btn = document.getElementById('btn');
+const names = [];
+
+button.addEventListener("click", () => {
+    block.innerHTML += input.value + '<br>';
+    names.push(input.value);
+    input.value = '';
+});
+
+
+
+
+btn.addEventListener("click", () => {
+    for (let i = 0; i < names.length; i++) {
+        if ([i] % 2 === 0) {
+            block3.innerHTML += names[i] + '<br>';
+        }
+        else {
+            block4.innerHTML += names[i] + '<br>';
+        }
+    }
+});
+
+
+// btn.addEventListener("click", () => {
+//     for (let i = 0; i < names.length; i++) {
+//         block2.innerHTML += names[i] + '<br>';
+//     }
+// });
